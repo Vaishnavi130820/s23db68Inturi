0e5273b919fd311376b0fcf912558d0c878752e8
Vaishnavi130820 <143207526+Vaishnavi130820@users.noreply.github.com>
2023-11-01 21:50:44 -0500

    added pictures

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var animalsRouter = require('./routes/animals');
-//var boardRouter = require('./routes/board');
+var boardRouter = require('./routes/board');
 //var chooseRouter = require('./routes/choose');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -24,7 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/animals', animalsRouter);
-// app.use('/board', boardRouter);
+app.use('/board', boardRouter);
 // app.use('/choose', chooseRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
