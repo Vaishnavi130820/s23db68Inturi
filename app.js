b327a974802a098efdcf381dadf8f4d029ef3687
Vaishnavi130820 <143207526+Vaishnavi130820@users.noreply.github.com>
2023-11-01 21:24:10 -0500

    added animals endpoint

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var animalsRouter = require('./routes/animals');
-var boardRouter = require('./routes/board');
-var chooseRouter = require('./routes/choose');
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// var boardRouter = require('./routes/board');
+// var chooseRouter = require('./routes/choose');
+// var indexRouter = require('./routes/index');
+// var usersRouter = require('./routes/users');
 
 var app = express();
 
@@ -24,8 +24,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/animals', animalsRouter);
-app.use('/board', boardRouter);
-app.use('/choose', chooseRouter);
+// app.use('/board', boardRouter);
+// app.use('/choose', chooseRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
